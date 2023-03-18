b5f3a37a8cd68e50ebe59b5114eab78aa50108c8
Керим Гучигов <forsyte12316gmail.com@MacBook-Air-Kerim.local>
2023-03-18 07:21:23 +0300

    решил аккермана

--- task68.cs
+++ task68.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Accerman(int m, int n)
+{
+    if (m==0)return n+1;
+    if (m>0 && n==0) return Accerman(m-1,1);
+    if (m>0 && n>0) return Accerman (m-1,Accerman(m, n-1));
+    else return -1;
+}
+
+System.Console.WriteLine("Введите M");
+int m = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Acc = Accerman(m,n);
+System.Console.WriteLine(Acc);
